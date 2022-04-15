8c17d58428bf703a23406c8dd6d7f0b7d192f7af
Eira Tabaniag <c20461286@mytudublin.ie>
2022-04-15 19:58:49 +0100

    Added comment headers

--- src/com/oop_assignment/Control.java
+++ src/com/oop_assignment/Control.java
@@ -1,11 +1,21 @@
 package com.oop_assignment;
 
+
+/**
+ * This Data Explorer program presents information and data
+ * from a data set of factors that would encourage greater use
+ * of public transport through GUI.
+ *
+ * Users can view the data through different filters
+ * and can also choose to see the data presented on
+ * a table or bar chart.
+ *
+ * @author Eira Tabaniag
+ */
 public class Control {
     public static void main(String[] args) {
-        //DBConnect db = new DBConnect();
+        // Instantiate MyGUI class
         MyGUI myGUI1 = new MyGUI();
-
-
 
     }
 }
